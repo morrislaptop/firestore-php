22d81d0438d5e70c209bef4c0c43f51bd8fc3b31
Tor Morten Jensen <tormorten@tormorten.no>
2021-06-22 21:34:14 +0200

    wip

--- src/References/Query.php
+++ src/References/Query.php
@@ -15,8 +15,9 @@ class Query
 {
     public function documents(array $options = [])
     {
-        $value = $this->apiClient->get($this->uri);
-        dd($this->uri . '?' . http_build_query($options));
+        $query = http_build_query($options);
+        $value = $this->apiClient->get($this->uri . ($query ? "?$query" : ''));
+
         $documents = new DocumentCollection();
 
         if (empty($value)) {
